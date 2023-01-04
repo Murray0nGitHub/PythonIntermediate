6d59e8ec79035c001f519c440702542f5b21477e
Austin Murray <115424250+Murray0nGitHub@users.noreply.github.com>
2023-01-03 23:41:49 -0500

    Initial commit

---
+++ Dictionaries.py
@@ -0,0 +1,26 @@
+#You are working at a car dealership and store the car data in a dictionary:
+#car = {
+#    'brand': 'BMW',
+#    'year': 2018,
+#    'color': 'red'
+#}
+
+#Your program needs to take the key as input and output the corresponding value.
+
+#Sample Input
+#year
+
+#Sample Output
+#2018
+#The data is already defined in the code.
+
+
+car = {
+    'brand':'BMW',
+    'year': 2018,
+    'color': 'red',
+    'mileage': 15000
+}
+
+a=input()
+print(car[a])
---
+++ List_Comprehensions.py
@@ -0,0 +1,15 @@
+#Given a word as input, output a list, containing only the letters of the word that are not vowels.
+#The vowels are a, e, i, o, u.
+
+#Sample Input
+#awesome
+
+#Sample Output
+#['w', 's', 'm']
+#Use a list comprehension to create the required list of letters and output it.
+
+
+word = input()
+vowels = ["a" , "e" , "i" , "o" , "u"]
+a = [i for i in word if i not in vowels]
+print(a)
