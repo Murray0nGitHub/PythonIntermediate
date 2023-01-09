a354fa7943f1b2df1abd7b6e686e330d6a32b506
Austin Murray <115424250+Murray0nGitHub@users.noreply.github.com>
2023-01-09 09:19:39 -0500

    update

---
+++ Working_With_Files.py
@@ -0,0 +1,15 @@
+#You are given a books.txt file, which includes book titles, each on a separate line.
+#Create a program to output how many words each title contains, in the following format:
+#Line 1: 3 words
+#Line 2: 5 words
+#...
+
+#Make sure to match the above mentioned format in the output.
+#To count the number of words in a given string, you can use the split() function, or, alternatively, count the number of spaces (for example, if a string contains 2 spaces, then it contains 3 words).
+
+
+with open("/usercode/files/books.txt") as f:
+   j = 1
+   for line in f:
+      print( "Line " +str(j)+ ": " + str(len(line.split(" "))) + " words")
+      j += 1
